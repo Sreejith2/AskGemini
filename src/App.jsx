ce99d8d7c735945ad0f3349e9b7597fe092014c6
Sreejith2 <99952612+Sreejith2@users.noreply.github.com>
2024-07-07 00:52:08 +0530

    2nd commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Main from './components/Main/Main'
 
 function App() {
   return (
-    <div className='flex md:overflow-clip max-h-screen'>
+    <div className='flex'>
       <Sidebar/>
       <Main/>
     </div>
