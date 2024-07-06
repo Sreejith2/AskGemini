672c310afcf25dd70d76ce60fe35e6592eab0ef1
Sreejith2 <99952612+Sreejith2@users.noreply.github.com>
2024-07-07 01:11:22 +0530

    4th commit

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -6,17 +6,17 @@ function Main() {
     const {onSent,recentPrompt,showResult,loading,resultData,setInput,input} = useContext(Context)
   return (
     <div className='flex flex-col flex-1 mt-4 min-h-screen relative'>
-        <div className='flex h-[10%] md:h-[50px] items-center text-green-900 text-[20px] px-3 justify-between min-w-full'>
+        <div className='flex h-[50px] md:h-[50px] items-center text-green-900 text-[20px] px-3 justify-between min-w-full'>
             <p>Gemini</p>
             <img className=' w-12 rounded-[50%] ' src={assets.user_icon} alt='' />
         </div>
-        <div className=' max-w-4xl m-auto '>
+        <div className=' max-w-4xl my-[16px] md:my-auto mx-auto '>
             <div className='min-h-[60vh]'>
                 {!showResult?<><div className='text-[25px] md:text-[35px] text-slate-700 font-[500] p-2'>
                     <p><span className=' bg-clip-text text-red-600 md:text-transparent md:bg-gradient-to-br from-red-500 to to-blue-500 ' >Hello , Dev</span></p>
                     <p>How can i help you today ?</p>
                 </div>
-                <div className='grid grid-cols-1 md:grid-cols-4 mb-3 md:mb-0 mx-6 md:mx-0 [&>div]:h-20  md:[&>div]:h-32 [&>div]:bg-slate-300 gap-2 md:[&>div>p]:text-[16px] [&>div>p]:text-[14px] [&>div>img]:bg-slate-100 [&>div>img]:rounded-[50%] [&>div]:relative [&>div]:p-4 [&>div]:rounded-lg [&>div>img]:w-6 [&>div>img]:absolute [&>div>img]:bottom-2 [&>div>img]:right-2'>
+                <div className='grid grid-cols-1 md:grid-cols-4 mb-2 md:mb-0 mx-6 md:mx-0 [&>div]:h-15  md:[&>div]:h-32 [&>div]:bg-slate-300 gap-2 md:[&>div>p]:text-[16px] [&>div>p]:text-[12px] [&>div>img]:bg-slate-100 [&>div>img]:rounded-[50%] [&>div]:relative [&>div]:p-4 [&>div]:rounded-lg [&>div>img]:w-6 [&>div>img]:absolute [&>div>img]:bottom-2 [&>div>img]:right-2'>
                     <div className=' hover:bg-slate-400 cursor-pointer '>
                         <p>Suggest beautiful places to visit.</p>
                         <img src={assets.compass_icon} alt='' />
@@ -51,7 +51,7 @@ function Main() {
             
         <div className='px-2 mx-auto md:mb-4'>
             <div className='flex items-center justify-between px-2 md:py-1 bg-slate-300 rounded-[50px] '>
-                <input onChange={(e) => setInput(e.target.value)} value={input} className='flex flex-1 bg-transparent outline-none border-none p-[2px] md:p-1 text-[14px] md:text-[16px] ' placeholder='Enter prompt here' />
+                <input onChange={(e) => setInput(e.target.value)} value={input} className='flex flex-1 bg-transparent outline-none border-none p-[2px] p-1 text-[14px] md:text-[16px] ' placeholder='Enter prompt here' />
                 <div className='flex items-center gap-3 '>
                     <img className=' w-4 md:w-6 cursor-pointer ' src={assets.gallery_icon} alt='' />
                     <img className=' w-4 md:w-6 cursor-pointer ' src={assets.mic_icon} alt='' />
